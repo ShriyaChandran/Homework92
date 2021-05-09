9d3a10ee27ed74334dfad654f83f3b2aff458292
ShriyaChandran <62100109+ShriyaChandran@users.noreply.github.com>
2021-05-09 09:50:13 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {TouchableOpacity, View, StyleSheet, TextInput} from 'react-native';
 import {createSwitchNavigator, createAppContainer } from 'react-navigation';
 import InputScreen from './screens/InputScreen';
+import firebase from 'firebase';
+import db from '../config'
 
 export default class App extends React.Component{
   render(){
